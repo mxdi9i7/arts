077cb52fbc8668110c0db1135520daa18607f593
Shengming Yuan <syua9708@flhs.us>
2017-09-17 10:58:40 +0800

    mail command

---
+++ email.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['email'])){
+    $to = "peter.zheng88228@gmail.com";
+    $fn = $_POST['first_name'];
+    $ln = $_POST['last_name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $send_message = "From ".$fn." ".$ln." / ".$email.", Message :".$message."";
+    mail($to,"Art Message From ".$fn."".$ln,$send_message);
+
+    echo"sent";
+}
+
+?>
